e4ef60856690a014552b435cc712f77566db1cd1
leighton-gr <leighton_grantham@epam.com>
2023-07-05 16:11:28 +0100

    Tries to fix build

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,4 @@
-/** @type {import('next-sitemap').IConfig} */
-module.exports = {
-  siteUrl: process.env.SITE_URL || 'https://saloboatservice.com',
-  generateRobotsTxt: true, // (optional)
-  // ...other options
-}
+// module.exports = {
+//   siteUrl: 'https://saloboatservice.com',
+//   generateRobotsTxt: true, // (optional)
+// }
